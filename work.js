6d15152e9a67d4428778ac49ec1399847336cbe4
bjjp0929 <hylin2146889@gmail.com>
2026-06-13 00:11:28 +0800

    Create work.js

---
+++ work.js
@@ -0,0 +1,28 @@
+const works = {
+  cake: {
+    title: "2024 Birthday Cake Event",
+    event: "Birthday Support",
+    time: "2024",
+    content: "Cake design / Event planning / Fan support",
+    image: "IMG_7580.jpg",
+    description: "這裡寫詳細介紹。"
+},
+
+  work2: {
+    title: "2024 Birthday Cake Event",
+    event: "Birthday Support",
+    time: "2024",
+    content: "Cake design / Event planning / Fan support",
+    image: "IMG_7580.jpg",
+    description: "這裡寫詳細介紹。"
+  },
+
+  work3: {
+    title: "2024 Birthday Cake Event",
+    event: "Birthday Support",
+    time: "2024",
+    content: "Cake design / Event planning / Fan support",
+    image: "IMG_7580.jpg",
+    description: "這裡寫詳細介紹。"
+  }
+};
