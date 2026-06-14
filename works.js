57246a80b2d285e1e25815eef61d5cfa01cf407f
bjjp0929 <hylin2146889@gmail.com>
2026-06-15 00:33:20 +0800

    Update works2(ad)

--- works.js
+++ works.js
@@ -8,12 +8,12 @@ const works = {
     description: "這裡寫詳細介紹。"
 },
 
-  work2: {
-    title: "2024 Birthday Cake Event",
-    event: "Birthday Support",
-    time: "2024",
-    content: "Cake design / Event planning / Fan support",
-    image: "IMG_7580.jpg",
+  ad: {
+    title: "2025 Birthday Subway Ad",
+    event: "Subway Ad",
+    time: "2025",
+    content: ["Ad design", "Event planning", "Fan support"],
+    image: "IMG_2720.WEBP",
     description: "這裡寫詳細介紹。"
   },
 
